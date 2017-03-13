fb16aec1ee11ff805d25b242c97deb1d85a30d4a
Josh Jones <Dugatsou@gmail.com>
2017-03-13 16:39:37 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,74 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+app.get("/faculty.html", function (request, response) {
+  response.sendFile(__dirname + '/views/faculty.html');
+});
+
+app.get("/find.html", function (request, response) {
+  response.sendFile(__dirname + '/views/find.html');
+});
+
+app.get("/wright.html", function (request, response) {
+  response.sendFile(__dirname + '/views/wright.html');
+});
+
+app.get("/cha.html", function (request, response) {
+  response.sendFile(__dirname + '/views/cha.html');
+});
+
+app.get("/street.html", function (request, response) {
+  response.sendFile(__dirname + '/views/street.html');
+});
+app.get("/salimian.html", function (request, response) {
+  response.sendFile(__dirname + '/views/salimian.html');
+});
+
+
+app.get("/courses.html", function (request, response) {
+  response.sendFile(__dirname + '/views/courses.html');
+});
+
+app.get("/notes.html", function (request, response) {
+  response.sendFile(__dirname + '/views/notes.html');
+});
+
+app.get("/table.html", function (request, response) {
+  response.sendFile(__dirname + '/views/table.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
